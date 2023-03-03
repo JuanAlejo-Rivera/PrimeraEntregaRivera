3924920c2b647992f4ed95b4eea32582e32b6c33
PrimeraEntrega <juan.alejorivera25@gmail.com>
2023-03-03 10:17:33 -0500

    Cambio Reentrega

--- PrimeraEntrega.js
+++ PrimeraEntrega.js
@@ -1,4 +1,3 @@
-const products = [];
 let reqid = 1;
 
 let TitleAdd;
@@ -18,37 +17,44 @@ class ProductManager {
         this.Id = Id;
         this.Stock = Stock;
     }
+    static products = [];
 
     //Metodos
     //Metodo nuevo producto
     addProduct() {
         const ProductNew = new ProductManager(
-            TitleAdd = "", //Ingrese el nombre del nuevo producto
-            DescAdd = "", //Ingrese la descripcion del nuevo producto
-            PriceAdd = parseInt("0"), //ingrese el precio del nuevo producto
-            ImgAdd = "", //ingrese extencion de imagen del producto
+            TitleAdd = "dgs", //Ingrese el nombre del nuevo producto
+            DescAdd = "sdgsd", //Ingrese la descripcion del nuevo producto
+            PriceAdd = parseInt("5"), //ingrese el precio del nuevo producto
+            ImgAdd = "sdfgsdg", //ingrese extencion de imagen del producto
             reqid += 1, //Ingrese id del producto
-            StockAdd = parseInt("0"), //Ingrese numero de stock del producto
+            StockAdd = parseInt("12"), //Ingrese numero de stock del producto
         )
         if (!TitleAdd == "" && !DescAdd == "" && !PriceAdd == 0 && !ImgAdd == "" && !StockAdd == 0) {
             return (ProductNew)
-        }else{
-            console.log("Todos los campos son requeridos, por favor verifique la información")
+        } else {
+            return ("Todos los campos son requeridos, por favor verifique la información")
         }
     }
 
     //Metodo que muestra los productos
     getProduct() {
-        return (products)
+        return (ProductManager.products)
     }
 
     //Buscador de productos
-    // getProductByid() {
-    //     let SearchId = "2"
-    //     if (products.match(/2/gi)) {
-    //         return (products)
-    //     }
-    // }
+    getProductByid(idb) {
+        let bool = false
+        ProductManager.products.forEach(element => {
+            if (element.Id == idb) {
+                console.log(element)
+                bool = true
+            }
+        });
+        if (!bool) {
+            console.log("No se encontro Id del producto")
+        }
+    }
 }
 
 const Product1 = new ProductManager(
@@ -56,24 +62,25 @@ const Product1 = new ProductManager(
     "Cerveza clasica hecha con los sabores tipos colombianos como el maiz y lúpulo",
     8000,
     "https://scontent.feoh4-4.fna.fbcdn.net/v/t39.30808-6/269623123_134127492385408_867247191007009107_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeHebtDhuABHISV9LVsxUHzcBSN9lz4YFnoFI32XPhgWekAcbPOVa8wKBykdx-Z655DI7dy13vlxtcDnO__q5Tkc&_nc_ohc=xC0cy5qutrcAX-yCTce&_nc_ht=scontent.feoh4-4.fna&oh=00_AfAVSmGMXuBQkkzitCBX_keX9ajbpYHUJzlW1Lm9pcyWww&oe=63FFD659",
-    reqid += 1,
+    5,
     17
 )
-products.push(Product1)
+ProductManager.products.push(Product1)
 
 const Product2 = new ProductManager(
     "Cerveza Mestiza",
     "Cerveza clasica hecha con los sabores tipos colombianos como el lúpulo y batata",
     8000,
     "https://ennuestrocampo.co/wp-content/uploads/2022/02/CervezadelValleSinu-Panzenu.jpg",
-    reqid += 1,
+    6,
     13
 )
-products.push(Product2)
+ProductManager.products.push(Product2)
 
 const newProduct = new ProductManager()
 
-products.push(newProduct.addProduct())
+ProductManager.products.push(newProduct.addProduct())
 
 
+newProduct.getProductByid(2)
 console.log(newProduct.getProduct())
